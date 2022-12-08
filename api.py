ace52cf25c6d45a0c9ede999ef4d8435932374da
Martin Oldfield <ex-git@mjoldfield.com>
2022-12-08 00:36:44 +0000

    Replaced mock hardware with code to drive Raspberry Pi /sys stuff.

--- api.py
+++ api.py
@@ -3,18 +3,45 @@ from fastapi.responses import FileResponse
 
 from pydantic import BaseModel
 
+import subprocess
+
+#
+# Define functions to talk to the hardware
+#
+# Here we use toy examples from /sys appropriate to
+# a Raspberry Pi.
+#
+# They might need root access, so run stuff in a shell
+# inside sudo.
+#
+def run_as_root(cmd):
+    x = subprocess.run(["sudo", "su", "-c", cmd]
+                       , capture_output=True
+                       , text=True
+                       , check=True)
+    return x.stdout.strip()
+
 def temp_read():
-    return { 'temperature': '123.45C' }
+    raw = run_as_root("cat /sys/class/thermal/thermal_zone0/temp")
+    t = f"{int(raw) / 1000.0:.1f}\N{DEGREE SIGN}C"
+    return { 'temperature': t }
 
 def led_init():
-    print("Initialized LEDs")
+    run_as_root("echo none > /sys/class/leds/led0/trigger")
 
 def led_set(x):
-    print(f"LED brightness({x})")
+    b = 0
+    if x > 0:
+        b = 255
+    run_as_root(f"echo {b} > /sys/class/leds/led0/brightness")
 
 def led_get():
-    return { 'brightness': 0.5 }
+    b = run_as_root("cat /sys/class/leds/led0/brightness")
+    return { 'brightness': b }
 
+#
+# The main HTTP server starts here
+#
 
 led_init()
 
